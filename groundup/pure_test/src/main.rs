0f066f9423539d3317bb0cc9458dd3f6d08b550d
Till Arnold <throwable42@gmail.com>
2023-10-03 15:39:31 +0200

    simpler pure

--- groundup/pure_test/src/main.rs
+++ groundup/pure_test/src/main.rs
@@ -3,11 +3,7 @@ use prusti_contracts::*;
 
 
 #[pure]
-#[ensures(result == 0)]
-fn len(v: i32) -> i32 { 0 }
+fn len() -> i32 { 0 }
 
-
-#[requires(len(vec) == 0)]
-fn client(vec: i32) {}
 
 fn main() {}
