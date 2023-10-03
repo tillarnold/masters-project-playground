902a43f95f73c30c4d9a1bdc8bbf435176117c1c
Till Arnold <throwable42@gmail.com>
2023-10-03 15:30:51 +0200

    1

--- groundup/ground1/src/main.rs
+++ groundup/ground1/src/main.rs
@@ -6,17 +6,29 @@ struct Vector {}
 impl Vector {
     #[pure]
     #[trusted]
-    fn len() -> i32 {
+    fn len(self) -> i32 {
         todo!()
     }
 
-    fn get(&self, idx: i32) -> i32 {
+    #[pure]
+    #[trusted]
+    #[requires(idx >= 0)]
+    #[requires(idx < self.len())]
+    fn get(self, idx: i32) -> i32 {
         todo!()
     }
 
+    #[trusted]
     fn set(self, idx: i32, value: i32) -> Self {
         todo!()
     }
+}
+
+
+#[requires(vec.len() == 10)]
+fn vector_client(vec: Vector) {
+    vec.get(3);
+
 }
 
 fn main() {
