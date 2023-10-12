610e36be94cf580d87256c5bd4048ecb4b9c1415
Till Arnold <throwable42@gmail.com>
2023-10-12 16:28:09 +0200

    test

--- groundup/pure_test/src/main.rs
+++ groundup/pure_test/src/main.rs
@@ -1,36 +1,42 @@
 extern crate prusti_contracts;
 use prusti_contracts::*;
 
-
-fn return_a_bool() ->  bool {
+fn return_a_bool() -> bool {
     true
 }
 
+#[pure]
 #[ensures(result >= 0)]
-fn len() -> i32 {
-    -1
+fn f1() -> i32 {
+    0
 }
 
-#[requires(i >= 0)]
-#[ensures(result >= i)]
-fn len2(i: i32) -> i32 {
-    i
-}
-
+#[pure]
 #[ensures(result == i)]
 fn id(i: i32) -> i32 {
     i
 }
 
-
-#[requires(i >= 0)]
-fn assert_ge_0(i: i32) {
-
+#[pure]
+#[ensures(result >= 0)]
+fn f2(b1: bool, b2: bool) -> i32 {
+    id(f1())
 }
 
+#[requires(i >= 0)]
+#[ensures(result >= i)]
+#[pure]
+fn len2(i: i32) -> i32 {
+    i
+}
+
+#[requires(i >= 0)]
+fn assert_ge_0(i: i32) {}
+
 fn main() {
-    let x = len();
+    let x = f1();
     let y = len2(x);
     let z = id(y);
     assert_ge_0(z);
 }
+
