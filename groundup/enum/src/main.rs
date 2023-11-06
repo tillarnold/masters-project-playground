252e88c790512d5f41f670c2c4314ac557ac2c42
Till Arnold <throwable42@gmail.com>
2023-11-06 19:36:59 +0100

    test

--- groundup/enum/src/main.rs
+++ groundup/enum/src/main.rs
@@ -5,9 +5,8 @@ enum Direction {
     Up,
     Down,
     Left,
-    Right
+    Right,
 }
-
 
 impl Direction {
     fn inverse(self) -> Direction {
@@ -18,24 +17,82 @@ impl Direction {
             Direction::Right => Direction::Left,
         }
     }
-} 
+}
 
+#[requires(b)]
+fn assert(b: bool) {}
 
+#[pure]
+fn is_up(dir: Direction) -> bool {
+    if let Direction::Up = dir {
+        return true;
+    }
+
+    return false;
+}
+
+fn test2(dir: Direction) {
+    let is_up = match dir {
+        Direction::Up => true,
+        _ => false,
+    };
+
+    let is_down = match dir {
+        Direction::Down => true,
+        _ => false,
+    };
+
+    assert(!is_up || !is_down);
+}
+
+fn test(dir: Direction) {
+    let other = match dir {
+        Direction::Up => Direction::Down,
+        _ => Direction::Up,
+    };
+
+    match other {
+        Direction::Left => assert(false),
+        _ => {}
+    };
+
+    let is_up = match other {
+        Direction::Up => true,
+        _ => false,
+    };
+
+    let is_down = match other {
+        Direction::Down => true,
+        _ => false,
+    };
+
+    assert(!is_up || !is_down);
+    assert(is_up || is_down);
+}
 
 enum IntOption {
     Some(i32),
-    None
+    None,
 }
 
 fn main() {
-    let x = Company {name: 42, address: 9};
+    let x = Person {
+        name: 42,
+        address: 9,
+    };
 
     let x = Direction::Down;
     let y = x.inverse();
+
+    let p = IntOption::Some(3);
+
+    match p {
+        IntOption::None => assert(false),
+        IntOption::Some(v) => assert(v == 3),
+    }
 }
 
-
-struct Company {
+struct Person {
     name: i32,
     address: u8,
 }
