e8f0bd4c31cd502cd8ba63318531ae3a602d9015
Till Arnold <throwable42@gmail.com>
2023-11-08 11:05:48 +0100

    test

--- groundup/enum/src/main.rs
+++ groundup/enum/src/main.rs
@@ -8,7 +8,20 @@ enum Direction {
     Right,
 }
 
+#[pure]
+#[trusted]
+fn eq(s: Direction, other: Direction) -> bool {
+    match (s, other) {
+        (Direction::Up, Direction::Up) => true,
+        (Direction::Down, Direction::Down) => true,
+        (Direction::Left, Direction::Left) => true,
+        (Direction::Right, Direction::Right) => true,
+        _unused => false,
+    }
+}
+
 impl Direction {
+    #[ensures(!(result === self))]
     fn inverse(self) -> Direction {
         match self {
             Direction::Up => Direction::Down,
@@ -17,19 +30,26 @@ impl Direction {
             Direction::Right => Direction::Left,
         }
     }
+    #[ensures((result === self))]
+    fn id(self) -> Direction {
+        match self {
+            Direction::Up => Direction::Up,
+            Direction::Down => Direction::Down,
+            Direction::Left => Direction::Left,
+            Direction::Right => Direction::Right,
+        }
+    }
 }
 
 #[requires(b)]
 fn assert(b: bool) {}
 
-#[pure]
-fn is_up(dir: Direction) -> bool {
-    if let Direction::Up = dir {
-        return true;
-    }
+#[requires(a === b)]
+fn assert_eq_dir(a: Direction, b: Direction) {}
 
-    return false;
-}
+
+#[requires(!(a === b))]
+fn assert_neq_dir(a: Direction, b: Direction) {}
 
 fn test2(dir: Direction) {
     let is_up = match dir {
@@ -75,14 +95,48 @@ enum IntOption {
     None,
 }
 
+impl IntOption {
+    #[pure]
+    #[trusted]
+    #[ensures(result == (self === IntOption::None))]
+    fn is_none(self) -> bool {
+        matches!(self, IntOption::None)
+    }
+    #[pure]
+    #[trusted]
+    #[ensures(result == !(self.is_none()))]
+    fn is_some(self) -> bool {
+        matches!(self, IntOption::Some(..))
+    }
+
+    #[ensures(self.is_none() ==> result == default)]
+    fn unwrap_or(self, default: i32) -> i32 {
+        match self {
+            IntOption::Some(v) => v,
+            IntOption::None => default,
+        }
+    }
+
+    #[pure]
+    fn none() -> IntOption {
+        IntOption::None
+    }
+
+    #[pure]
+    fn some(i: i32) -> IntOption {
+        IntOption::Some(i)
+    }
+}
 fn main() {
     let x = Person {
         name: 42,
         address: 9,
+        facing: Direction::Up,
     };
 
     let x = Direction::Down;
-    let y = x.inverse();
+    let y = x.inverse().id();
+    assert_neq_dir(y, Direction::Down);
 
     let p = IntOption::Some(3);
 
@@ -90,9 +144,16 @@ fn main() {
         IntOption::None => assert(false),
         IntOption::Some(v) => assert(v == 3),
     }
+
+    let v = IntOption::None.unwrap_or(22);
+    assert(v == 22);
+
+    IntOption::none();
+    IntOption::some(3);
 }
 
 struct Person {
     name: i32,
     address: u8,
+    facing: Direction,
 }
