e2829a928232056f9101d5606c8a1c3f0013d9e8
Till Arnold <throwable42@gmail.com>
2023-11-11 11:30:15 +0100

    test

--- groundup/enum-repack-test/src/main.rs
+++ groundup/enum-repack-test/src/main.rs
@@ -3,8 +3,6 @@
 extern crate prusti_contracts;
 use prusti_contracts::*;
 
-
-
 #[requires(b)]
 fn assert(b: bool) {}
 
@@ -13,19 +11,16 @@ enum IntOption {
     None,
 }
 
-impl IntOption {
-    #[rustc_mir(borrowck_graphviz_postflow="log/analysis/fn_name/fn_name.dot")]
-    #[ensures(self === IntOption::None ==> result == default)]
-    fn unwrap_or(self, default: i32) -> i32 {
-        match self {
-            IntOption::Some(v) => v,
-            IntOption::None => default,
-        }
+#[rustc_mir(borrowck_graphviz_postflow = "log/analysis/fn_name/fn_name.dot")]
+#[ensures(s === IntOption::None ==> result == default)]
+fn unwrap_or(s: IntOption, default: i32) -> i32 {
+    match s {
+        IntOption::Some(v) => v,
+        IntOption::None => default,
     }
-
 }
+
 fn main() {
-    let v = IntOption::None.unwrap_or(22);
+    let v = unwrap_or(IntOption::None, 22);
     assert(v == 22);
 }
-
