ce87b83925e33a4d226f2a1184195b944de61d2c
Till Arnold <throwable42@gmail.com>
2024-01-15 08:35:30 +0100

    test

--- groundup/vec_test_carbon/src/main.rs
+++ groundup/vec_test_carbon/src/main.rs
@@ -1,24 +1,13 @@
 extern crate prusti_contracts;
 use prusti_contracts::*;
 
-pub struct Vector {
-    len: i32,
-    contents: i32, // dummy variable encoding the contents of the vector
-}
 
-impl Vector {
-    #[pure]
-    #[trusted]
-    fn get(self, idx: i32) -> i32 {
-        unimplemented!()
-    }
-}
+#[pure]
+fn id(i: i32)-> i32 {i}
 
-
-
-#[ensures(forall(|i: i32|  result.get(i) === data.get(i)))]
-fn apply_id_rec_s(data: Vector, idx: i32) -> Vector {
-    data
+#[ensures(forall(|i: i32| id(i) === id(i)))]
+fn apply_id_rec_s(idx: i32) -> i32 {
+    1
 }
 
 
