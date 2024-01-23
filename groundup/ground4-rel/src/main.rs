b01d65c6ca78537644b6aa19aedb77af9ce9fbc0
Till Arnold <throwable42@gmail.com>
2024-01-23 11:29:20 +0100

    rel

---
+++ groundup/ground4-rel/src/main.rs
@@ -0,0 +1,350 @@
+extern crate prusti_contracts;
+use prusti_contracts::*;
+
+pub struct Set {
+    contents: u32,
+}
+
+impl Set {
+    #[pure]
+    #[trusted]
+    fn contains(&self, el: i32) -> bool {
+        unimplemented!()
+    }
+
+    #[pure]
+    #[trusted]
+    #[ensures(forall(|el: i32| (result.contains(el) <==>  (self.contains(el) | other.contains( el) ) ) ))]
+    fn union(&self, other: Set) -> Set {
+        unimplemented!()
+    }
+
+    #[pure]
+    #[trusted]
+    // TODO 
+    fn card(&self) -> usize {
+        unimplemented!()
+    }
+
+    #[pure]
+    #[trusted]
+    #[ensures(result.contains(el))]
+    #[ensures(result.card() == 1)]
+    #[ensures(forall(|e: i32| (result.contains(e) <==>  e == el ) ))]
+    fn singleton(el: i32) -> Set{
+        unimplemented!()
+    }
+}
+
+
+
+
+// function empty_mapping() : Mapping 
+//     ensures keys(result) == Set()
+//     ensures values(result) == Set()
+
+#[pure]
+#[trusted]
+#[ensures(result ==> (
+    m.keys().card() == m.values().card()  && 
+    (forall(|v : i32|  (m.values().contains(v) ==> m.translate(m.translate_invert(v)) == v)))
+
+))]
+fn valid_mapping(m: &Map) -> bool
+{
+    unimplemented!()
+}
+
+
+#[pure]
+fn mapps(m: &Map, k: i32, v: i32) -> bool {
+      m.keys().card() == m.values().card() && m.keys().contains(k) && m.values().contains(v) && m.translate(k) == v && m.translate_invert(v) == k
+}
+
+struct Map {
+    contents: u32,
+}
+
+
+impl Map {
+    #[pure]
+    #[trusted]
+    fn keys(&self) -> Set {
+        unimplemented!()
+    }
+
+    #[pure]
+    #[trusted]
+    fn values(&self) -> Set {
+        unimplemented!()
+    }
+
+    #[pure]
+    #[trusted]
+    fn translate(&self, el: i32) -> i32 {
+        unimplemented!()
+    }
+
+    #[pure]
+    #[trusted]
+    fn translate_invert(&self, el: i32) -> i32 {
+        unimplemented!()
+    }
+
+    #[pure]
+    #[trusted]
+    #[requires(!(self.keys().contains(k)))]
+    #[requires(!(self.values().contains(v)))]
+    #[requires(self.keys().card() == self.values().card())]
+    #[ensures((result.keys() === ((self.keys()).union(Set::singleton(k)))))]
+    #[ensures((result.values() === ((self.values()).union(Set::singleton(v)))))]
+    #[ensures(result.translate(k) == v)]
+    #[ensures(result.translate_invert(v) == k)]
+    // #[ensures(forall kp: Int :: (kp in keys(m) && kp != k) ==> translate(result, kp) == translate(m, kp))]
+    // #[ensures(translate_invert(result, v) == k)]
+    // #[ensures(forall vp: Int :: (vp in values(m) && vp != v) ==> translate_invert(result, vp) == translate_invert(m, vp))]
+    fn insert(self, k: i32, v: i32) -> Map  {
+        unimplemented!()
+
+    }
+
+  
+
+}
+
+#[requires(valid_mapping(&m))]
+#[requires(!(m.keys().contains(k)))]
+#[requires(!(m.values().contains(v)))]
+#[ensures(valid_mapping(&result))]
+fn insert_client(m: Map, k: i32, v: i32)  -> Map 
+ {
+     let a = m.insert(k,v);
+     assert_true(mapps(&a, k, v));
+     a
+}
+
+pub struct Vector {
+    len: usize,
+    contents: i32, // dummy variable encoding the contents of the vector
+}
+
+impl Vector {
+    #[pure]
+    #[trusted]
+    #[requires(idx >= 0)]
+    #[requires(idx < self.len)]
+    #[requires(self.len >= 0)]
+    fn get(self, idx: usize) -> i32 {
+        unimplemented!()
+    }
+
+    #[trusted]
+    #[requires(idx >= 0)]
+    #[requires(idx < self.len)]
+    #[requires(self.len >= 0)]
+    #[ensures(result.0 == self.get(idx))]
+    #[ensures(result.1 === self)]
+    fn impure_get(self, idx: usize) -> (i32, Self) {
+        unimplemented!()
+    }
+
+    #[trusted]
+    #[requires(idx >= 0)]
+    #[requires(idx < self.len)]
+    #[requires(self.len >= 0)]
+    #[ensures(self.len == result.len)]
+    #[ensures(result.get(idx) == value)]
+    #[ensures(forall(|i : usize| (i >= 0  && i < self.len && !(i == idx)) ==> result.get(i) == self.get(i)))]
+    fn set(self, idx: usize, value: i32) -> Self {
+        unimplemented!()
+    }
+}
+
+
+
+struct Bounds {
+    upper: i32,
+    lower: i32,
+}
+
+struct ClampTransform {
+    bounds: Bounds,
+}
+
+#[requires(data.len >= 0)]
+#[requires(transform.bounds.lower < transform.bounds.upper)]
+#[ensures(result.0.len === data.len)]
+#[ensures(result.1 === transform)]
+#[ensures(forall(|ip: usize| (0<= ip) & (ip < data.len) ==> result.0.get(ip) == transform.do_transform(data.get(ip))))]
+fn apply_row_by_row(transform: ClampTransform, data: Vector/*<i32>*/) -> (Vector/*<i32>*/, ClampTransform) {
+    if data.len <= 0 {
+        return (data, transform);
+    }
+
+    let l = data.len;
+    assert_true( l >= 1);
+    assert_true( l - 1 >= 0);
+    assert_true( l - 1 < l);
+    apply_row_by_row_rec(transform, data, l - 1)
+}
+
+#[requires(transform.bounds.lower < transform.bounds.upper)]
+#[requires(idx >= 0)]
+#[requires(data.len >= 1)]
+#[requires(idx < data.len)]
+#[ensures(result.0.len === data.len)]
+#[ensures(result.1 === transform)]
+#[ensures(forall(|i: usize| ((i >= 0) && (i > idx) && (i < data.len)) ==> result.0.get(i) == data.get(i)))]
+#[ensures(forall(|i: usize| ((i >= 0) && (i <= idx) && (i < data.len)) ==> result.0.get(i) == transform.do_transform(data.get(i))))]
+fn apply_row_by_row_rec(transform: ClampTransform, data: Vector/*<i32>*/, idx: usize) -> (Vector/*<i32>*/, ClampTransform) {
+    let (modified, transform) = if idx >= 1 {
+        apply_row_by_row_rec(transform, data, idx - 1)
+    } else {
+        (data, transform)
+    };
+
+    let (cur, data) = modified.impure_get(idx);
+    let (new, transform) = transform.do_transform_impure(cur);
+    let modified = data.set(idx, new);
+    (modified, transform)
+}
+
+
+#[requires(idx < 1000)]
+#[requires(data.len == 2000)]
+#[requires(idx >= 0)]
+#[requires(data.len >= 1)]
+#[requires(idx < data.len)]
+#[ensures(result.len === data.len)]
+// #[ensures(forall(|i: usize| (i >= 0 && i < data.len) ==> ( result.get(i) === data.get(i))  ))] 
+#[ensures(forall(|i: usize| (i >= 0 && i > idx && i < data.len)  ==> result.get(i) === data.get(i)))]
+#[ensures(forall(|i: usize| (i >= 0 && i <= idx && i < data.len)  ==> result.get(i) === data.get(i)))]
+fn apply_id_rec(data: Vector, idx: usize) -> Vector {
+    if idx + 1 < 10 {
+        apply_id_rec(data, idx + 1)
+    } else {
+        data
+    }
+}
+
+
+
+// #[requires(idx >= 0)]
+// #[requires(data.len >= 1)]
+// #[requires(idx < data.len)]
+// #[ensures(result.len === data.len)]
+// #[ensures(forall(|i: usize| ((i >= 0) && (i < data.len)) ==> result.get(i) == (data.get(i))))]
+// // #[ensures(forall(|i: usize| ((i >= 0) && (i > idx) && (i < data.len)) ==> result.get(i) == data.get(i)))]
+// // #[ensures(forall(|i: usize| ((i >= 0) && (i <= idx) && (i < data.len)) ==> result.get(i) == (data.get(i))))]
+// fn id_rec(data: Vector, idx: usize) -> Vector {
+//     let (modified) = if idx >= 1 {
+//         id_rec(data, idx - 1)
+//     } else {
+//         (data)
+//     };
+
+//     let (cur, data) = modified.impure_get(idx);
+//     let modified = data.set(idx, cur);
+//     (modified)
+// }
+
+
+
+#[requires(i >= 0)]
+fn assert_geq_0(i: i32) {}
+
+#[pure]
+fn max(a: i32, b: i32) -> i32 {
+    if a > b {
+        a
+    } else {
+        b
+    }
+}
+
+#[pure]
+fn min(a: i32, b: i32) -> i32 {
+    if a > b {
+        b
+    } else {
+        a
+    }
+}
+
+impl ClampTransform {
+    #[ensures(result.bounds === bounds)]
+    fn make_clamp(bounds: Bounds) -> Self {
+        Self { bounds }
+    }
+
+    #[pure]
+    #[requires(self.bounds.lower < self.bounds.upper)]
+    fn do_transform(self, data: i32) -> i32 {
+        // implemented like this due to limitation in pure controll flow
+        max(self.bounds.lower, min(self.bounds.upper, data))
+    }
+
+    #[requires(self.bounds.lower < self.bounds.upper)]
+    #[ensures(result.0 == self.do_transform(data))]
+    #[ensures(result.1 === self)]
+    fn do_transform_impure(self, data: i32) -> (i32, Self) {
+        if data < self.bounds.lower {
+            (self.bounds.lower, self)
+        } else if data > self.bounds.upper {
+            (self.bounds.upper, self)
+        } else {
+            (data, self)
+        }
+    }
+}
+
+#[requires(b)]
+fn assert_true(b: bool) {}
+
+#[requires(a == b)]
+fn assert_eq(a: i32, b: i32) {}
+
+
+#[requires(a === b)]
+fn assert_eq_snap(a: Vector, b: Vector) {}
+
+#[requires(vec.len == 10)]
+fn vector_client(vec: Vector) {
+    let vec = vec.set(5, 42);
+    let res = vec.get(5);
+    assert_eq(res, 42);
+}
+
+#[pure]
+fn between(val: i32, lower: i32, upper: i32) -> bool {
+    val <= upper && val >= lower
+}
+
+#[requires(forall(|i: usize| (0<= i && i< res.len) ==> between(res.get(i), lower, upper)))]
+fn final_assert(res: Vector, lower: i32, upper: i32) {}
+
+#[requires(data.len >= 10)]
+#[requires(lower < upper)]
+pub fn client(data: Vector, lower: i32, upper: i32) {
+    let t = ClampTransform::make_clamp(Bounds { lower, upper });
+    let (res, t2) = apply_row_by_row(t, data);
+
+    final_assert(res, lower, upper);
+    //prusti_assert!(forall(|i: i32| (0<= i && i< res.len) ==> res.get(i) <= 200 && res.get(i) >= 100))
+}
+
+
+/// This function shows that the second field in vector is required
+/// if that field is removed this verifies implying that all vectors of the same length are the same
+#[requires(data1.len == data2.len)]
+#[requires(data1 !== data2)]
+#[cfg(f)]
+pub fn client2(data1: Vector, data2: Vector) {
+    assert_true(false);
+}
+
+
+
+fn main() {
+
+}
