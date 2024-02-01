5fdb1e262d1d3fb93354c2521dc6db927abdd855
Till Arnold <throwable42@gmail.com>
2024-02-01 17:02:40 +0100

    test

---
+++ groundup/running-ex/clamp-primitive/src/main.rs
@@ -0,0 +1,25 @@
+extern crate prusti_contracts;
+use prusti_contracts::*;
+
+
+#[pure]
+fn clamp(value: i32, min: i32, max: i32) -> i32 {
+    if value < min {
+        min
+    }
+    else if value > max {
+        max
+    }
+    else {
+        value
+    }
+}
+
+#[requires(clamp(i, 10, 20) == 20)]
+fn clamp_user(i: i32) {
+
+}
+
+fn main() {
+
+}
