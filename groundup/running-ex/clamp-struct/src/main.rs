d21c5d1bcbbe1e89d16d1dd2e435f0cb67252463
Till Arnold <throwable42@gmail.com>
2024-02-10 19:29:36 +0100

    [running]

--- groundup/running-ex/clamp-struct/src/main.rs
+++ groundup/running-ex/clamp-struct/src/main.rs
@@ -1,35 +1,30 @@
 extern crate prusti_contracts;
 use prusti_contracts::*;
 
-
 struct Bounds {
     min: i32,
     max: i32,
 }
-// #[pure]
-// #[requires(bounds.min <= bounds.max)]
-// #[ensures(result<=bounds.max)]
-// #[ensures(result>=bounds.min)]
-// #[ensures((value<=bounds.max && value >= bounds.min) ==> result == value)]
-// fn clamp(value: i32, bounds: Bounds) -> i32 {
-//     if value < bounds.min {
-//         bounds.min
-//     }
-//     else if value > bounds.max {
-//         bounds.max
-//     }
-//     else {
-//         value
-//     }
-// }
 
-// #[requires(clamp(i, Bounds{min: 10, max: 20}) == 20)]
-// fn clamp_user(i: i32) {
+#[requires(bounds.min <= bounds.max)]
+#[ensures(result<=bounds.max)]
+#[ensures(result>=bounds.min)]
+#[ensures((value<=bounds.max && value >= bounds.min) ==> result == value)]
+fn clamp(value: i32, bounds: Bounds) -> i32 {
+    if value < bounds.min {
+        bounds.min
+    } else if value > bounds.max {
+        bounds.max
+    } else {
+        value
+    }
+}
 
-// }
+#[ensures(result == 20)]
+fn clamp_user(i: i32) -> i32 {
+    clamp(i, Bounds{min: 10, max: 20})
+}
 
 fn foo(u: Bounds) {}
 
-fn main() {
-
-}
+fn main() {}
